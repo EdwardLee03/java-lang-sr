19d7ee25850b0f71c779da59a54200b95b2229ae
Edward Lee <bert825_work@163.com>
2014-09-17 14:51:59 +0800

    [译] java.lang包描述
    本包提供Java编程语言设计的基础类，Object和Class是最重要的两个类。
    Object类是类层次结构树的根，Class实例表示正在运行的Java应用程序的类和接口。
    
    使用对象表示基本数据类型的值是有必要的，包装器类(wrapper classes)就为这个目标服务。
    常用的包装器类有Integer、Long。
    
    Math类提供常用的数学函数。
    String、StringBuffer和StringBuilder类相似地提供字符串的常用操作。
    
    ClassLoader、Process、ProcessBuilder、Runtime、SecurityManager和System类提供"系统操作"，用来管理类型(classes)的动态加载、外部进程的创建、主机环境的查询和安全策略的执行。
    
    Throwable类封装了可能被throw语句抛出的对象，Throwable子类可以表示错误(errors)和异常(exceptions)。
    
    [字符编码]
    Charset类的规范描述字符编码的命名规范和一组标准的编码。

--- src/java/lang/package-info.java
+++ src/java/lang/package-info.java
@@ -4,7 +4,29 @@
  */
 
 /**
- * Provides classes that are fundamental to the design of the Java
+ * 本包提供Java编程语言设计的基础类，Object和Class是最重要的两个类。
+ * Object类是类层次结构树的根，Class实例表示正在运行的Java应用程序的类和接口。
+ * 
+ * <p>使用对象表示基本数据类型的值是有必要的，包装器类(wrapper classes)就为这个目标服务。
+ * 常用的包装器类有Integer、Long。例如，一个Integer类型的对象通过包含一个int类型的字段来表示值，
+ * 这样它就可以被存储在一个引用类型的变量中。
+ * 这些类还提供在基本类型之间转换的一些方法，同时还支持如equals和hashCode这样的标准方法。
+ * 
+ * <p>Math类提供常用的数学函数。
+ * String、StringBuffer和StringBuilder类相似地提供字符串的常用操作。
+ * 
+ * <p>ClassLoader、Process、ProcessBuilder、Runtime、SecurityManager和System类
+ * 提供"系统操作"，用来管理类型(classes)的动态加载、外部进程的创建、主机环境的查询和
+ * 安全策略的执行。
+ * 
+ * <p>Throwable类封装了可能被throw语句抛出的对象，
+ * Throwable子类可以表示错误(errors)和异常(exceptions)。
+ * 
+ * <h3>字符编码</h3>
+ * 
+ * Charset类的规范描述字符编码的命名规范和一组标准的编码。
+ * 
+ * <p><p>Provides classes that are fundamental to the design of the Java
  * programming language. The most important classes are {@code
  * Object}, which is the root of the class hierarchy, and {@code
  * Class}, instances of which represent classes at run time.
